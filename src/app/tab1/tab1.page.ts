26faf0b3dbb16811119ebd405a6dc6ebc8b464e7
Ali Ok <aliok@apache.org>
2018-12-21 11:52:11 +0300

    Initial import

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import gql from 'graphql-tag';
+import {VoyagerClient, createClient} from '@aerogear/datasync-js';
+
+const HELLO = gql`
+    query hello {
+        hello
+    }
+`;
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+    async voyager() {
+        console.log('voyager');
+
+        const apollo: VoyagerClient = await this.createApolloClient();
+
+        try {
+            const foo = await apollo.query<any>({
+                query: HELLO,
+                fetchPolicy: 'network-only',
+                errorPolicy: 'none'
+            });
+
+            console.log('success xx');
+            console.log(foo);
+        } catch (e) {
+            console.log('error xx');
+            console.log(e);
+        }
+    }
+
+    public async createApolloClient() {
+        const self = this;
+        // const uri = 'http://localhost:4000/graphql';
+        const uri = 'http://192.168.1.111:4000/graphql';
+        // const wsUri = 'ws://localhost:4000/graphql';
+        return await createClient({
+            httpUrl: uri,
+            auditLogging: true
+            // wsUrl: wsUri
+        });
+    }
+}
